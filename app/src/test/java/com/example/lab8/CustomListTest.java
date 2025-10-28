94220254feae31c4ab0c522addba55a3d3616ab1
Michelle Tran <mttran@ualberta.ca>
2025-10-27 22:01:18 -0600

    add

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,27 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    /**
+     * create a mocklist for citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    /**
+     * get the size of the list
+     * increase list by adding new city
+     * check f current size matches initial size plus 1
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize +1);
+    }
 
 }
